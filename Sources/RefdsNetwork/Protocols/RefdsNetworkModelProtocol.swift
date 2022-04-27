c8825c5e1bbc7438601c09edddb8a34db6cdce49
Rafael Santos <40582668+rafaelesantos@users.noreply.github.com>
2022-04-27 15:40:07 -0400

    Change model protocol functions

--- Sources/RefdsNetwork/Protocols/RefdsNetworkModelProtocol.swift
+++ Sources/RefdsNetwork/Protocols/RefdsNetworkModelProtocol.swift
@@ -11,19 +11,19 @@ import Combine
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
 public protocol RefdsNetworkModelProtocol: Codable {
-    func getEndpoint() -> RefdsNetworkEndpoint<Self>
-    func getRequestData() -> RefdsNetworkRequestDataProtocol
-    func load() -> AnyPublisher<Self, Error>
+    static func getEndpoint() -> RefdsNetworkEndpoint<Self>
+    static func getRequestData() -> RefdsNetworkRequestDataProtocol
+    static func load() -> AnyPublisher<Self, Error>
 }
 
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
 public extension RefdsNetworkModelProtocol {
-    func getRequestData() -> RefdsNetworkRequestDataProtocol {
+    static func getRequestData() -> RefdsNetworkRequestDataProtocol {
         return RefdsNetworkRequestData(method: .get, headers: [.accept(.applicationJson)])
     }
     
-    func load() -> AnyPublisher<Self, Error> {
+    static func load() -> AnyPublisher<Self, Error> {
         return RefdsNetwork.shared.load(for: getEndpoint(), using: getRequestData())
     }
 }
