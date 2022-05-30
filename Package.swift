39b4cf10aef4884d65d3a345665fe4bca4e515af
Rafael Santos <rafael.escaleira@hotmail.com>
2022-05-30 18:40:56 -0400

    Add swift tools versions

--- Package.swift
+++ Package.swift
@@ -1,3 +1,5 @@
+// swift-tools-version: 5.4
+// swift-tools-version: 5.5
 // swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
