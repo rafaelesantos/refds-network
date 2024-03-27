4dd93dff32c5283d4e6379cedf069fb7628c85fe
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-03-27 09:35:32 -0400

    Update RefdsHttpRequest.swift

--- Sources/RefdsNetwork/Domain/Http/RefdsHttpRequest.swift
+++ Sources/RefdsNetwork/Domain/Http/RefdsHttpRequest.swift
@@ -4,16 +4,16 @@ import RefdsShared
 public protocol RefdsHttpRequest {
     associatedtype Response
     
-    var httpClient: RefdsHttpClient { get set }
-    var httpEndpoint: RefdsHttpEndpoint { get set }
+    var httpClient: RefdsHttpClient { get }
+    var httpEndpoint: RefdsHttpEndpoint? { get }
     
-    func decode(_ data: Data, endpoint: RefdsHttpEndpoint?) throws -> Response
+    func decode(_ data: Data) throws -> Response
 }
 
 public extension RefdsHttpRequest where Response: RefdsModel {
-    func decode(_ data: Data, endpoint: RefdsHttpEndpoint?) throws -> Response {
+    func decode(_ data: Data) throws -> Response {
         guard let decoded: Response = data.asModel() else { throw RefdsHttpError.invalidResponse(content: data) }
-        endpoint?.logger()
+        httpEndpoint?.logger()
         decoded.logger()
         return decoded
     }
